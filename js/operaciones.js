7c26f14d4cac6ed48b5f13dc63d1d5aefc54dbd7
Manu <manusasson@gmail.com>
2023-07-31 20:32:09 -0300

    Agregar al carrito

--- js/operaciones.js
+++ js/operaciones.js
@@ -1,16 +1,31 @@
 function agregarAlCarrito(id) {
     let articulo = articulos.find((articulo) => articulo.id == id);
+    let carrito = [];
+    carrito.push(articulo)
+
     console.log(articulo);
     localStorage.setItem("Carrito", JSON.stringify(articulo));
     let respuesta = document.getElementById("respuesta");
-    respuesta.innerHTML = `<p>Su producto fue agregado al carrito <button class="btn btn-dark" onClick="obtenerProducto()">Ver Producto</button></p>`;
+
+
+
+      // 
+      Swal.fire({
+        title: '¡Felicitaciones!',
+        text: `El producto ${articulo.nombre} fue agregado al carrito!`,
+        icon: 'success', // Puedes usar 'success', 'error', 'warning', 'info' o 'question'
+        confirmButtonText: 'Aceptar'
+    });
+      //mostrarMensaje();
+
+    respuesta.innerHTML = `<button class="btn btn-dark" onClick="obtenerProducto()">Ver Carrito</button></p>`;
   }
   
   function obtenerProducto() {
     let producto = JSON.parse(localStorage.getItem("Carrito"));
     let salida = `<div class="col-md-4">
-      <img class="img-fluid" src="${producto.imagen}" alt="${producto.titulo}" onClick="agregarAlCarrito(${producto.id})">
-       <p>${producto.titulo} <br/> ${producto.precio} </p>
+      <img class="img-fluid" src="${producto.imagen}" alt="${producto.nombre}" onClick="agregarAlCarrito(${producto.id})">
+       <p>${producto.nombre} <br/> ${producto.precio} </p>
       </div>`;
     let recuperar = document.getElementById("recuperar");
     recuperar.innerHTML = salida;
